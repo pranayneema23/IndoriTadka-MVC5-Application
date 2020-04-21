13370810f59af4c87a59921008285a9e82d0b4ad
Pranay Neema <neema.pranay23@gmail.com>
2020-04-21 16:13:07 +0530

    Added IrestaurentData.cs and InMemoryRestaurantData.cs file in Services folder

---
+++ IndoriTadka.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,27 @@
+﻿using IndoriTadka.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IndoriTadka.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+                {
+                    new Restaurant { Id =1, Name = "Laal balti", Cuisine = CuisineType.Indian},
+                    new Restaurant { Id =2, Name = "Pizza World", Cuisine = CuisineType.Italina},
+                    new Restaurant { Id =3, Name = "Bombay Chinese ", Cuisine = CuisineType.Chinese}
+                };
+        }
+
+        IEnumerable<Restaurant> IRestaurantData.GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
