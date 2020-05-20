d227d252182d70172366307f268610017c44a383
Pranay Neema <neema.pranay23@gmail.com>
2020-05-20 23:39:54 +0530

    Add QueryString concept comment

--- IndoriTadka.Web/Controllers/GreetingController.cs
+++ IndoriTadka.Web/Controllers/GreetingController.cs
@@ -14,8 +14,14 @@ namespace IndoriTadka.Web.Controllers
         public ActionResult Index(string name)
         {
             var model = new GreetingViewModel();
+            //var nameQueryString = HttpContext.Request.QueryString["name"];
+            //Generally when we work with MVC5 you do not need to access HttpContext property directly
+            //You just add variable in action method as argument and MVC5 automatically find from QueryString
             model.Message = ConfigurationManager.AppSettings["message"];
-            model.Name = name ?? "Anonymous";
+            model.Name = name ?? "Anonymous";// ?? is null collation operator
+            //ASP.Net also apply some built-In protection to prevent script tag into queryString
+            //Also RazorView also provide one more aditional security layer and display
+            //Script string as plain text
             return View(model);
         }
     }
