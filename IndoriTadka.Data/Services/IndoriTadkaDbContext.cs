12e4d3cf5f18229bfc55471fec3db003436e3bc4
Pranay Neema <neema.pranay23@gmail.com>
2020-05-25 23:45:11 +0530

    Add SqlRestaurentData.cs and DbContext class

---
+++ IndoriTadka.Data/Services/IndoriTadkaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using IndoriTadka.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndoriTadka.Data.Services
+{
+    //To access data inside DB use EF base class DbContext
+    //DbContext is gatway to any database
+    public class IndoriTadkaDbContext : DbContext
+    {
+        //Now IndoriTadkaDbContext is a getway to db for this project
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ IndoriTadka.Data/Services/SqlRestaurantData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IndoriTadka.Data.Models;
+using System.Data.Entity;
+
+namespace IndoriTadka.Data.Services
+{
+    class SqlRestaurantData : IRestaurantData
+    {
+        private readonly IndoriTadkaDbContext db;
+
+        public SqlRestaurantData(IndoriTadkaDbContext db)
+        {
+            this.db = db;
+        }
+
+        public void Add(Restaurant restaurant)
+        {
+            db.Restaurants.Add(restaurant);
+            db.SaveChanges();
+        }
+
+        public Restaurant Get(int id)
+        {
+            return db.Restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in db.Restaurants
+                   orderby r.Name
+                   select r;
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            var entry = db.Entry(restaurant);
+            entry.State = EntityState.Modified;
+            db.SaveChanges();
+
+        }
+    }
+}
